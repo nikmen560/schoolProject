5fc8a4873db03bc5bed17ab9cba235f2c320e674
Kent <Kent720p@gmail.com>
2021-08-02 14:56:06 +0300

    created class Teacher

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,11 @@
+public class Teacher {
+    private String name;
+    private String lastName;
+    private String surname;
+
+    public Teacher(String name, String lastName, String surname) {
+        this.name = name;
+        this.lastName = lastName;
+        this.surname = surname;
+    }
+}
