ca8291e231f820a4cdde9f6a7c936408bf10d12b
Kent <Kent720p@gmail.com>
2021-08-02 14:50:21 +0300

    created 2 classes StudentGroup and StudyProgram

---
+++ src/main/java/StudentGroup.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class StudentGroup {
+    private int groupNumber;
+    private int studyYears;
+    private List<Subject> subjects;
+    private List<Student> students;
+
+
+}
---
+++ src/main/java/StudyProgram.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.Map;
+
+public class StudyProgram {
+    private StudentGroup studentGroup;
+    private Map<Semester, List<Subject>> subjectsForSemester;
+
+
+}
