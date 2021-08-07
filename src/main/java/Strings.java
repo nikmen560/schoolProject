842a7f7afb76c5e3f373c76018072b58e6eda987
Kent <Kent720p@gmail.com>
2021-08-07 19:29:21 +0300

    created class Strings

---
+++ src/main/java/Strings.java
@@ -0,0 +1,39 @@
+public class Strings {
+    public static final String HELLO_DISPLAY = "Welcome to iStudent";
+    public static final String MAIN_MENU = """
+            Choose option by typing a number:
+            1. Students\s
+            2. Student groups\s
+            3. Subjects\s
+            4. Teachers\s""";
+
+    public static final String STUDENT_MENU = """
+            1. Show all students\s
+            2. Add student\s
+            3. Update student\s
+            4. Remove Student\s""";
+
+    public static final String STUDENT_MENU_ADD_STUDENT_NAME = "Enter name";
+    public static final String STUDENT_MENU_ADD_STUDENT_SURNAME = "Enter surname";
+    public static final String STUDENT_MENU_ADD_STUDENT_STUDENT_NUMBER = "Enter student number";
+    public static final String STUDENT_MENU_ADD_STUDENT_GROUP_NUMBER = "Enter group number";
+
+    public static final String STUDENT_MENU_UPDATE_STUDENT_NAME = "Enter name";
+    public static final String STUDENT_MENU_UPDATE_STUDENT_SURNAME = "Enter surname";
+    public static final String STUDENT_MENU_UPDATE_STUDENT_STUDENT_NUMBER = "Enter student number";
+    public static final String STUDENT_MENU_UPDATE_STUDENT_GROUP_NUMBER = "Enter group number";
+
+    public static final String STUDENT_MENU_REMOVE_STUDENT = "Enter the id of the student";
+    public static final String STUDENT_MENU_REMOVE_STUDENT_SUCCESS = "The student has been removed";
+    public static final String STUDENT_MENU_REMOVE_STUDENT_FAIL = "The student has not been removed";
+
+
+    public static final String STUDENT_GROUPS_MENU_ADD_GROUP_NAME = "Enter name";
+    public static final String STUDENT_GROUPS_MENU_ADD_GROUP_NUMBER = "Enter number";
+    public static final String STUDENT_GROUPS_MENU_ADD_GROUP_SEMESTER = "Enter semester";
+
+    public static final String STUDENT_GROUPS_MENU_UPDATE_GROUP = "Enter name";
+    public static final String STUDENT_GROUPS_MENU_REMOVE_GROUP = "Enter name";
+
+
+}
